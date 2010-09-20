1d6e59373d5213c0ba5bb1dd8fcf834c4459bf95
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2010-09-20 04:54:45 +0200

    Add support for pure-python extensions
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- cmake/modules/PythonExtension.cmake
+++ cmake/modules/PythonExtension.cmake
@@ -13,17 +13,28 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-function(python_extension extension_NAME extension_PREFIX)
+function(python_extension extension_NAME extension_PREFIX install_PREFIX)
+  set(impure TRUE)
+  foreach(arg ${ARGN})
+    if(${arg} STREQUAL "PURE")
+      set(impure FALSE)
+    endif(${arg} STREQUAL "PURE")
+  endforeach(arg)
+
   # find source files
   file(GLOB ${extension_NAME}_SOURCES ${extension_PREFIX}/*.cxx)
   file(GLOB ${extension_NAME}_PYTHON ${extension_PREFIX}/*.py)
 
-  # build the extension
-  add_library(${extension_NAME} MODULE ${${extension_NAME}_SOURCES})
-  target_link_libraries(${extension_NAME} ${Boost_PYTHON_LIBRARY})
-  set_target_properties(${extension_NAME} PROPERTIES PREFIX "_")
+  # build the extension, if necessary
+  if(impure)
+    add_library(${extension_NAME} MODULE ${${extension_NAME}_SOURCES})
+    target_link_libraries(${extension_NAME} ${Boost_PYTHON_LIBRARY})
+    set_target_properties(${extension_NAME} PROPERTIES PREFIX "_")
 
-  # install it
-  install(TARGETS ${extension_NAME} LIBRARY DESTINATION ${extension_PREFIX})
-  install(FILES ${${extension_NAME}_PYTHON} DESTINATION ${extension_PREFIX})
+    # install it
+    install(TARGETS ${extension_NAME} LIBRARY DESTINATION ${install_PREFIX}${extension_PREFIX})
+  endif(impure)
+
+  # install the python part
+  install(FILES ${${extension_NAME}_PYTHON} DESTINATION ${install_PREFIX}${extension_PREFIX})
 endfunction(python_extension)

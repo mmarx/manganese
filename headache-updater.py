f39fd3b49831db75f4391c0fa35fb8bba296d765
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2011-02-10 18:39:37 +0100

    Simplify headache-updater logic, avoiding spurious blank lines
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- headache-updater.py
+++ headache-updater.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python
 
-from sys import argv, exit
 from subprocess import Popen
 
-invert = False
-error = True
-
-if len(argv) > 1 and argv[1] == 'remove':
-    invert = True
-    error = False
-elif len(argv) > 1 and argv[1] == 'update':
-    error = False
-
-if error:
-    print 'usage: %s (update|remove)' % argv[0]
-    exit(1)
-    
 cfg = {'config': 'headache.conf',
        'header': 'headache.header',
        'types': ['py',                  # python
@@ -24,11 +10,15 @@ cfg = {'config': 'headache.conf',
                  ],
        'find': r"find . -regex '%(regex)s' -exec %(command)s \;",
        'headache': 'headache -c %(config)s -h %(header)s %(invert)s {}',
-       'invert': '-r' if invert else ''
+       'invert': '-r'
        }
 
-cfg['regex'] = r'.*\.\(' + r'\|'.join(cfg['types']) + r'\)'
-cfg['command'] = (cfg['headache'] % cfg)
-
 if __name__ == '__main__':
+    # first, remove the old headers
+    cfg['regex'] = r'.*\.\(' + r'\|'.join(cfg['types']) + r'\)'
+    cfg['command'] = (cfg['headache'] % cfg)
+    Popen (cfg['find'] % cfg, shell=True)
+    # now, apply the new ones
+    cfg['invert'] = ''
+    cfg['command'] = (cfg['headache'] % cfg)
     Popen (cfg['find'] % cfg, shell=True)

3d837b29d122f55ce1e1fccc20b11dbb3b9232ab
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2011-03-18 19:45:38 +0100

    WIP: Geometry generation
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

---
+++ vismut/vismut/gl/geometry.py
@@ -0,0 +1,15 @@
+
+
+from __future__ import division
+
+from OpenGL import GL
+
+import numpy
+
+
+def circle(center, radius, subdivisions=3):
+    def subdivide(from, to, subdivisions):
+        pass
+
+    pass
+

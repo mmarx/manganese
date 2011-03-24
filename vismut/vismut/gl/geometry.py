69874386da0014598b7d0eadb23dc35af5e42c4f
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2011-03-24 16:43:54 +0100

    Finished circle geometry generation
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- vismut/vismut/gl/geometry.py
+++ vismut/vismut/gl/geometry.py
@@ -13,25 +13,43 @@ from manganese.math.vector import Vec2
 
 def circle(center, radius, subdivisions=3, z=1.5, scale=None):
     translate = Vec2(center)
-    r = radius * sin(pi/4)
+    r = radius * sin(pi / 4)
 
-    if scale:
+    if scale is not None:
         x, y = r * scale[0], r * scale[1]
     else:
         x, y = r, r
-    
+
     vertices = [[-x, -y, z],
                 [x, -y, z],
                 [-x, y, z],
+                [x, -y, z],
                 [x, y, z],
+                [-x, y, z],
                 ]
-    
-    def subdivide(from, to, subdivisions):
-        base = -from + to
-        new = translate + base.normal.normalized * radius
 
-        subdivide(from, new, subdivisions - 1)
+    def subdivide(fr, to, subdivisions):
+        if subdivisions <= 0:
+            return
+
+        base = -fr + to
+        new = translate - base.normal.normalized * radius
+
+        if scale is not None:
+            new = Vec2(new[0] * scale[0],
+                       new[1] * scale[1])
+
+        vertices.extend([[fr[0], fr[1], z],
+                         [new[0], new[1], z],
+                         [to[0], to[1], z],
+                         ])
+
+        subdivide(fr, new, subdivisions - 1)
         subdivide(new, to, subdivisions - 1)
 
-    pass
+    subdivide(Vec2(-x, -y), Vec2(x, -y), subdivisions)
+    subdivide(Vec2(x, -y), Vec2(x, y), subdivisions)
+    subdivide(Vec2(x, y), Vec2(-x, y), subdivisions)
+    subdivide(Vec2(-x, y), Vec2(-x, -y), subdivisions)
 
+    return vertices

2018a840479bb51a250c3325e536ec1477457822
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2013-04-19 00:58:04 +0200

    Add more midi event types
    Signed-off-by: Maximilian Marx <mmarx@wh2.tu-dresden.de>

--- manganese/midi/jack/classifier.py
+++ manganese/midi/jack/classifier.py
@@ -1,17 +1,17 @@
 ########################################################################
 # manganese - midi analysis & visualization platform
 # Copyright (c) 2010, 2011, 2013 Maximilian Marx <mmarx@wh2.tu-dresden.de>
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 2 of
 # the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -23,6 +23,12 @@
 class EventClassifier(object):
     _map = {0x80: 'note off',
             0x90: 'note on',
+            0xa0: 'key aftertouch',
+            0xb0: 'controller change',
+            0xc0: 'program change',
+            0xd0: 'channel pressure',
+            0xe0: 'pitch change',
+            0xf0: 'system exclusive'
             }
 
     def __getitem__(self, key):
